3062ce9d56f935df09e005a638d01595484afdc8
VAJJAPALLI-ARUN <vajjapilliarun@gmail.com>
2025-02-25 14:33:48 +0530

    initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/c9/c005b06125f3001f1691aaefd56daf3c
@@ -0,0 +1,8 @@
+package com.arun.sample;
+import java.util.*;
+public class WAP_9 {
+	public static void main(String []args) {
+		
+	}
+
+}
--- Sample/src/com/arun/sample/WAP_9.java
+++ Sample/src/com/arun/sample/WAP_9.java
@@ -2,6 +2,18 @@ package com.arun.sample;
 import java.util.*;
 public class WAP_9 {
 	public static void main(String []args) {
+		int c=0, mc=0;
+		public int isPrime(int a);
+		{
+			for(int i=2;i<a/2;i++)
+			{
+				if(a%i==0)
+					c++;
+			}
+			if(c==0)
+				mc++;
+		}
+		Scanner s=new Scanner(System.in);
 		
 	}
 
